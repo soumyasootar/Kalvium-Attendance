697e05cc5658abec230b2aad8abf6a8a977a7f4f
soumyasootar <soumyaswaroopsootar@gmail.com>
2023-10-15 16:49:34 +0530

    Chakra UI integrated

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,18 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import { Providers } from './providers'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+import Checkin from '@/components/Checkin'
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      HOME PAGE
+      <Providers>
+        <Navbar/>
+        <Checkin/>
+        <Footer/>
+      </Providers>
     </main>
   )
 }
---
+++ frontend/src/components/Checkin.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Checkin = () => {
+  return (
+    <div>Checkin Page</div>
+  )
+}
+
+export default Checkin
